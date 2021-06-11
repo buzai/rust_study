fd35dfeb0f24f151ed2dbda79ce5dfcef2f3a6f9
chenhao <783243077@qq.com>
2021-06-11 12:02:57 +0800

    rust function

--- function/target/rls/debug/deps/function-244cce8510c18658.d
+++
@@ -1,5 +0,0 @@
-/Users/chenhao/code/rust.projects/rust_study/function/target/rls/debug/deps/function-244cce8510c18658.rmeta: src/main.rs
-
-/Users/chenhao/code/rust.projects/rust_study/function/target/rls/debug/deps/function-244cce8510c18658.d: src/main.rs
-
-src/main.rs:
