7f10051e9ffabe5f032c882db24a97ba9c3740f8
chenhao <783243077@qq.com>
2021-06-28 16:51:08 +0800

    types

--- types/src/main.rs
+++ types/src/main.rs
@@ -26,7 +26,35 @@ fn main() {
     let a = {
         1 + 1
     };
-    println!("a: {}", a)
+    println!("a: {}", a);
+
+    let a1: u32 = "42".parse().expect("not number");
+    println!("a1: {}", a1);
+
+    // 2^n -1 ~ 2^n-1 -1 / 0 ~ 2^n
+    let f1 = 0.1; // f1 is f64 
+    let f2 : f32 = 0.1; // f2 f32
+
+    let b1 = true;
+    let mut b2: bool;
+    b2 = false;
+    b2 = true;
+    println!("b2 {}", b2);
+
+
+    let t = (1, 2);
+    let (t1, t2)=t;
+    // t.0 t.1
+
+    let arrr = [1,24];
+
+    let arrrrr : [u32; 5];
+    let arrrrrr = [3; 50];
+
+    println!("{}", arrrrrr[30]);
+    println!("{}", arrrrrr[49]);
+
+
 }
 
 fn print_arr(a: [i32;2]) {
